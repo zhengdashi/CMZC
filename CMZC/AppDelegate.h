e2d8d9d4362b1aa502616fd6d5eaa9b13be545b8
财毛 <caimao@caimaodemac-mini.local>
2016-03-01 11:09:45 +0800

    Initial Commit

---
+++ CMZC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMZC
+//
+//  Created by 财毛 on 16/3/1.
+//  Copyright © 2016年 财毛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CMZC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CMZC
+//
+//  Created by 财毛 on 16/3/1.
+//  Copyright © 2016年 财毛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
